2e7c6dee8b41db7a55eb89bfc7147b5c2a864f6b
yezhouguo <32995811+yezhouguo@users.noreply.github.com>
2022-05-12 13:37:45 +0800

    Update __init__.py

--- njupass/__init__.py
+++ njupass/__init__.py
@@ -26,7 +26,7 @@ class NjuUiaAuth:
     def __init__(self):
         self.session = requests.Session()
         self.session.headers.update({
-            'User-Agent': "Mozilla/5.0 (Linux; Android 11; M2006J10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36  cpdaily/8.2.7 wisedu/8.2.7"，
+            'User-Agent': "Mozilla/5.0 (Linux; Android 11; M2006J10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36  cpdaily/8.2.7 wisedu/8.2.7",
             "Referer": "http://ehallapp.nju.edu.cn/xgfw/sys/mrjkdkappnju/index.html",
             # "Host": "ehallapp.nju.edu.cn",
             "X-Requested-With":"com.wisedu.cpdaily.nju",
